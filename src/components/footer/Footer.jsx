5df12a9229fd5caa2dd2ecd0055f791a0aedbd71
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-26 21:57:53 +0300

    added how component, not finished styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,24 +12,24 @@ import { Grid } from '@mui/material';
 
 export default function Footer() {
     return (
-        <Grid container spacing={5} className={classNames(styles.gridContainer)}
+        <Grid container spacing={5} className={classNames(styles.footContainer)} style={{margin:'0px'}}
             flexDirection='row'
             justifyContent="center"
             alignItems="center"
-            style={{ padding: '10px' }}>
+            >
 
             <Grid item
                 xs={3} sm={3} md={2}
                 className={classNames(styles.logo)}
-                style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                style={{ display: 'flex', justifyContent: 'flex-end', padding:'0'}}>
                 <Logo type='muted'/>
             </Grid>
 
-            <Grid item xs={4} sm={4} md={4} justifyContent='center'>
+            <Grid item xs={4} sm={4} md={4} justifySelf='center' style={{padding:'0'}}>
               <p className={classNames(styles.text)}>Bum All Rights Reserved 2021</p>
             </Grid>
 
-            <Grid item xs={4} sm={4} md={4} style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            <Grid item xs={4} sm={4} md={4} style={{ display: 'flex', justifyContent: 'space-evenly', padding:'0' }}>
                 <ButtonBase size='large'
                     color='secondary'
                     className={classNames(styles.whiteButton)}>Privacy Policy
