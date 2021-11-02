481a2af01f8612e7926a3b07504c29a7b66f8722
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-02 08:22:20 +0200

    added aditional scss module

--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -3,24 +3,23 @@ import { Container } from "@mui/material";
 import Collector from "./Collector";
 import styles from './CollectorColumn.module.scss';
 import classNames from "classnames";
-import { style } from "@mui/material/node_modules/@mui/system";
 
 export default function CollectorColumn({ items }) {
 
     return (
-        <div>
+        <div className={classNames(styles.collectorColumn)}>
             {items.map((crritem, crrIndex) => {
                 
                 return (
                     <div key={`${crrIndex}_${crritem.name}`}>
-                        <p>{crrIndex + 1}</p>
                         <Collector
                             name={crritem.name}
                             nftsCount={crritem.nftsCount}
                             avatar={crritem.avatar}
                             verified={crritem.verified}
-                            key={crritem.name}
+                            id={crrIndex}
                             type={crrIndex % 2 !== 0 ? 'light': ''}
+                            key={crritem.name}
                         ></Collector>
                     </div>
                 );
