79b29287fde6e032f6e2e1c87e329e4bf567556b
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-09 23:28:26 +0200

    added product info likes

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -3,6 +3,7 @@ import ProductImage from "../../src/components/product/ProductImage";
 import ProductInfoTitle from "../../src/components/product/ProductInfoTitle";
 import ProductInfoPrice from "../../src/components/product/ProductInfoPrice";
 import ProductInfoStatus from "../../src/components/product/ProductInfoStatus";
+import ProductInfoLikes from "../../src/components/product/ProductInfoLikes";
 import dataNft from '../data/nfts.json';
 import Head from '../../src/components/header/Header';
 import Footer from '../../src/components/footer/Footer';
@@ -26,6 +27,7 @@ export default function Product() {
         <ProductInfoTitle text={'Wooden pants'} />
         <ProductInfoPrice amount={3} currency={'ETH'} />
         <ProductInfoStatus />
+        <ProductInfoLikes amount={2500}/>
         <ProductImage 
         url={'https://nft-auction.herokuapp.com/uploads/0xa5192c2be7bf2603bbbae09c340dfeac07275a7a_93a8d3aa6f.jpg'} />
         <Footer />
