88ffd3aec54206df224ab87fbb943bd9fa2a0cc3
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-15 08:25:55 +0200

    added table row classses

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./ProductTabs.module.scss";
+import * as styles from "./ProductTabs.module.scss";
 import classNames from "classnames";
 import User from "../user/User";
 import { formatDistance } from "date-fns";
@@ -33,7 +33,7 @@ export default function ProductTabs({ text, bids }) {
                 <TableBody>
                     {
                         bids.map((crrBid, i) => {
-                            return <TableRow className={classNames(styles[`table-row-${i}`])} key={`${crrBid.user.name} bid`} >
+                            return <TableRow className={classNames(styles[`table-row-${i}`])} key={`${crrBid.user.name} bid`}>
                                 <TableCell component='th' scope='row'>
                                     <User name={crrBid.user.name} avatar={crrBid.user.avatar} verified={crrBid.user.verified} />
                                 </TableCell>
@@ -54,15 +54,16 @@ export default function ProductTabs({ text, bids }) {
         </TableContainer>
 
     return (
-        <TabContext  value={value}>
-            <TabList onChange={handleChange} className={classNames(styles["product-tabs"])}>
-                <Tab label='DETAILS' value="1" className={classNames(styles["tab-details"])} />
-                <Tab label='BIDS' value="2" className={classNames(styles["tab-bids"])}/>
-            </TabList>
-            <TabPanel value="1">{text}</TabPanel>
-            <TabPanel value="2">{currentBids}</TabPanel>
+            <TabContext  className={styles["product-tabs"]} value={value} >
+                <TabList onChange={handleChange} >
+                    <Tab label='DETAILS' value="1" className={classNames(styles["tab-details"])} />
+                    <Tab label='BIDS' value="2" className={classNames(styles["tab-bids"])} />
+                </TabList>
+                <TabPanel value="1">{text}</TabPanel>
+                <TabPanel value="2">{currentBids}</TabPanel>
 
+            </TabContext>
 
-        </TabContext>
+        
     )
 }
