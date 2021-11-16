b5645f21e55e20ce168febeea0a541f8e9005b73
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-16 20:07:34 +0200

    trying with column

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,7 +21,7 @@ import ProductInfoCreator from "../src/components/product/ProductInfoCreator";
 import ProductInfoTimer from "../src/components/product/ProductInfoTimer";
 import ProductInfo from "../src/components/product/ProductInfo";
 import ProductTabs from "../src/components/product/ProductTabs";
-import ProductActions from "../src/components/product/ProductActions"
+import ProductActions from "../src/components/product/ProductActions";
 import Link from "next/link";
 
 export default function Index() {
