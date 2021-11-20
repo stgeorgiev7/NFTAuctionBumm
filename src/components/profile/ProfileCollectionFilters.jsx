053048a831022afa23e385321f59590088f90417
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-20 03:18:01 +0200

    added imput props as obj

--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -46,7 +46,7 @@ export default function ProfileCollectionFilters({ filters }) {
                     </FormControl>
 
                     <FormControl>
-                        <TextField type='search' variant="standard" InputProps={{variant='standard'}}>
+                        <TextField type='search' variant="standard" InputProps={{variant:'standard'}}>
                             <InputAdornment position='start' >
                                 <Search className={classNames(styles.searchIcon)}></Search>
                             </InputAdornment>
