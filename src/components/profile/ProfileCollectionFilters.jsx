cf373dbdc7731a3a066a2ae41871b08b7db014f7
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-20 02:42:09 +0200

    added ProfileCollectionFilters component

---
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import styles from "./ProfileCollectionFilters.module.scss"
+import classNames from "classnames";
+import { Container } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { Select } from "@mui/material";
+import { InputLabel } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import { Stack } from "@mui/material";
+import { TextField } from "@mui/material";
+import { InputAdornment } from "@mui/material";
+import { Search } from "@mui/icons-material";
+
+export default function ProfileCollectionFilters({ filters }) {
+
+    const [sortValues, setSortValues] = useState(filters.sort);
+    const [priceValues, setPriceValues] = useState(filters.price);
+
+    console.log(sortValues);
+
+    console.log(filters);
+
+    return (
+        <div className={classNames(styles["profile-collection-filters"])}>
+            <Container>
+                <Stack direction={"row"} className={classNames(styles["stack"])}>
+                    <h1 className={classNames(styles["title"])}>Collection</h1>
+
+                    <FormControl>
+
+                        <Select value={0}>
+                            <MenuItem value={0} style={{ display: 'none' }}>Sort by</MenuItem>
+                            {sortValues.map(el => {
+                                return <MenuItem key={el.label} value={el.value}>{el.label}</MenuItem>
+                            })}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl>
+                        <Select value={0}>
+                            <MenuItem value={0} style={{ display: 'none' }}>Price range</MenuItem>
+                            {priceValues.map(el => {
+                                return <MenuItem key={el.label} value={el.value}>{el.label}</MenuItem>
+                            })}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl>
+                        <TextField type='search'  variant='filled'>
+                            <InputAdornment position='start' >
+                                <Search className={classNames(styles.searchIcon)}></Search>
+                            </InputAdornment>
+                        </TextField>
+                    </FormControl>
+
+                </Stack>
+            </Container>
+        </div>
+    )
+}
--- src/components/profile/ProfileUser.jsx
+++ src/components/profile/ProfileUser.jsx
@@ -8,9 +8,9 @@ export default function ProfileUser({name, info, avatar, verified}) {
 
     return (
         <div className={classNames(styles["profile-user"])}>
-            <Avatar url={avatar} size={{width: 100, height: 100}} verified={verified} />
-            <Typography variant={"h3"} children={name} />
-            <Typography variant={"body1"} children={info} />
+            <Avatar url={avatar} size={{width: 150, height: 150}} verified={verified} />
+            <Typography variant={"h3"} children={name} className={classNames(styles.typo)}/>
+            <Typography variant={"body1"} children={info} className={classNames(styles.typo)} />
         </div>
     )
 }
