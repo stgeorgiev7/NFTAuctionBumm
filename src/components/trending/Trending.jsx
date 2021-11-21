b34ec50d8873dc8307377917396d86aed5c1fa59
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-21 18:16:57 +0200

    added data url to trending component

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -7,8 +7,13 @@ import { Container } from '@mui/material';
 import { Select } from '@mui/material';
 import { MenuItem } from '@mui/material';
 
-export default function Trending({ cards = [] }) {
+export default function Trending({ cards = [], filters }) {
     const [data, setData] = useState(cards);
+    const [valueSort, setValueSort] = useState('');
+
+    function handleSort(e) {
+        setValueSort(e.target.value);
+    };
 
     const cardsData = cards.map(function (nft, index) {
         return (
@@ -40,10 +45,15 @@ export default function Trending({ cards = [] }) {
 
             <Grid item xs={7} sm={7} md={7} justifyContent='flex-end' style={{display:'flex', padding:0, alignSelf:'center'}}>
 
-                <Select value={10} className={classNames(styles.select)} sx={{":hover": {border:'none'}}} >
-                    <MenuItem value={10}>This Week</MenuItem>
-                    <MenuItem value={20}>This Month</MenuItem>
-                    <MenuItem value={30}>This Year</MenuItem>
+                <Select
+                 value={0}
+                 className={classNames(styles.select)} 
+                 sx={{":hover": {border:'none'}}}
+                 >
+                    <MenuItem value={0} style={{ display: 'none' }}>This Week</MenuItem>
+                    {filters?.sort?.map((item, i) => {
+                            return <MenuItem key={i} value={item.value}>{item.label}</MenuItem>
+                    })}
                 </Select>
             </Grid>
 
